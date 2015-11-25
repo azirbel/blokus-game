f69c06183ce655f3224e914ca9b5d438acc01ba3
Alex Zirbel <alexzirbel@gmail.com>
2015-11-25 15:40:04 -0500

    Placeholder for Pygame GUI

--- game.py
+++ game.py
@@ -1,16 +1,34 @@
 from move import Move
 from game_state import GameState
 from game_utils import *
+from gui import Gui
+import argparse
 import player_dominic.player as player1
 import player_random.player as player2
 import player_random.player as player3
 import player_random.player as player4
+import sys
+import time
+
+parser = argparse.ArgumentParser(description='Run the blokus game.')
+parser.add_argument('--gui',
+    dest='gui',
+    action='store_true',
+    help='use the pygame gui')
+args = parser.parse_args()
+
+if args.gui:
+    gui = Gui()
 
 passes = 0
 state = GameState()
 while passes < 4:
     player = state.turn % 4
 
+    if args.gui:
+        gui.update()
+
+    print('Player ' + str(player) + '\'s turn!')
     if player == 1:
         move = player1.getMove(state.clone())
     elif player == 2:
@@ -26,9 +44,12 @@ while passes < 4:
         passes = 0
     
     try:
+        print('  > Move made: ' + str(move))
         state.applyMove(move)
     except InvalidMove:
         print('Invalid move by player ' + str(player) + ':')
         print('   ' + str(move))
+
+    time.sleep(1)
 
 # TODO scoring
